53c9a92ed039a1d8806a6649aebc6d4d9b57c7f1
rollg <maildevcom@gmail.com>
2017-11-28 17:52:18 +0200

    Vuex has been added and configured;

--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
+/* Twitter Bootstrap JS (this could also be handled in an app.js file) */
+require('bootstrap')
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+import store from './store'
+import VueResource from 'vue-resource'
 
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
+/* App sass */
+import './assets/style/app.scss'
+
+/* App component */
+import App from './App.vue'
+
+/* Auth plugin */
+import Auth from './auth'
+
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
 new Vue({
   el: '#app',
+  // Attach the Vue instance to the window,
+  // so it's available globally.
+  created: function () {
+    window.Vue = this
+  },
   router,
-  template: '<App/>',
-  components: { App }
+  store,
+  render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,62 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
+    // Each of these routes are loaded asynchronously, when a user first navigates to each corresponding endpoint.
+    // The route will load once into memory, the first time it's called, and no more on future calls.
+    // This behavior can be observed on the network tab of your browser dev tools.
+    {
+      path: '/login',
+      name: 'login',
+      component: function (resolve) {
+        require(['@/components/login/Login.vue'], resolve)
+      }
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: function (resolve) {
+        require(['@/components/signup/Signup.vue'], resolve)
+      }
+    },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'dashboard',
+      component: function (resolve) {
+        require(['@/components/dashboard/Dashboard.vue'], resolve)
+      }
     }
   ]
 })
+
+function guardRoute (to, from, next) {
+  // work-around to get to the Vuex store (as of Vue 2.0)
+  const auth = router.app.$options.store.state.auth
+
+  if (!auth.isLoggedIn) {
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+}
+
+export default router
+
+// import HelloWorld from '@/components/HelloWorld'
+//
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
